597f943b169aecc9f00088523aa59e9a7cf54d9d
Sergii Kasian <kasyansv1@gmail.com>
2019-07-08 20:13:40 +0300

    Updates

---
+++ precache-manifest.9c9294cbae6f940cd4af5a259a062038.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71c9ee797ff59f040e54b1b5d1b05bc",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "fdfc65952d23b6a10781",
+    "url": "/react_static-list-of-posts/static/css/main.1e905930.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-list-of-posts/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "fdfc65952d23b6a10781",
+    "url": "/react_static-list-of-posts/static/js/main.6f974561.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
